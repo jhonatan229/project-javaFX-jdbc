496f698d7412e751df267b8796e036bd6fc5dfdb
jhonatan estevam <jhonatan229@outlook.com>
2020-02-27 18:34:32 -0300

    saving department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,45 +3,77 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.service.DepartmentService;
 
-public class DepartmentFormController implements Initializable{
+public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-	
+
+	private DepartmentService service;
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	@FXML
 	private TextField textFieldName;
-	
+
 	@FXML
 	private TextField textFieldId;
-	
+
 	@FXML
 	private Label labelError;
-	
+
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("save");
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("service was null");
+		}
+		try {
+			entity = getFormDate();
+			service.updateOrSave(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+
 	}
-	
+
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("cancel");
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
-	
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
+	private Department getFormDate() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(textFieldId.getText()));
+		obj.setName(textFieldName.getText());
+		return obj;
 	}
 
 	@Override
@@ -52,7 +84,7 @@ public class DepartmentFormController implements Initializable{
 	private void initializeNode() {
 		Constraints.setTextFieldInteger(textFieldId);
 		Constraints.setTextFieldMaxLength(textFieldName, 30);
-		
+
 	}
 
 	public void updateFormData() {
